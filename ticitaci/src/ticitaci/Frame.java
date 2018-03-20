4c1f1d71ef4aad15588920ad9db50aee78fa3a08
Peter <trompeten.peter@gmx.de>
2018-03-20 21:48:39 +0100

    sdfg

---
+++ ticitaci/src/ticitaci/Frame.java
@@ -0,0 +1,12 @@
+package ticitaci;
+
+import javax.swing.JFrame;
+
+public class Frame extends JFrame {
+	public Frame() {
+		setSize(800, 800);
+		setVisible(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
